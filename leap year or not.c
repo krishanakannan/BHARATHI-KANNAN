471a8b6ecf3991df507d11680ce36bf671d809ff
krishanakannan <1998starkannan@gmail.com>
2017-09-25 06:54:07 -0700

    Update leap year or not.c

--- leap year or not.c
+++ leap year or not.c
@@ -4,7 +4,15 @@ int main()
 int year;
 printf("\nEnter the year");
 scanf("%d",&year);
-if((year%4)==0)
+if((year%400)==0)
+{
+printf("%d is leap year \n");
+}
+else if((year%100)==0)
+{
+printf("%d is not leap year \n");
+}
+else if((year%4)==100)
 {
 printf("%d is leap year \n");
 }

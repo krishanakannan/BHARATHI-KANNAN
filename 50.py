78a1d70abaffed0153ff6cc1a952a43d2baf8920
krishanakannan <1998starkannan@gmail.com>
2018-02-23 09:40:42 +0530

    Create 50.py

---
+++ 50.py
@@ -0,0 +1,25 @@
+def pot(a):
+
+    if(a == 0):
+
+        return False
+
+    while (a!=1):
+
+            if((a%2)!=0):
+
+                return False
+
+            a=a//2
+
+    return True
+
+n=input()
+
+if(pot(n)):
+
+    print("yes")
+
+else:
+
+    print("no")

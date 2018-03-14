0a77695dd39197c3063ee83fef283eb87e9ed78c
krishanakannan <1998starkannan@gmail.com>
2018-03-14 15:30:44 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int k,m,a;
+printf("enter the num");
+scanf("%d",&k);
+printf("enter the left and right limit");
+scanf("%d%d",&m,&a);
+if((k>m)&&(k<a))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}

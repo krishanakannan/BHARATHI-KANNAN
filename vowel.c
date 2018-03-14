a1704cbb83065ec1b5795527552bff96d89c43ee
krishanakannan <1998starkannan@gmail.com>
2018-03-14 15:33:09 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -4,7 +4,7 @@ int main()
 char a[50];
 int i=0;
 printf("Enter the string:");
-scanf("%s",a);
+scanf("%s",&a);
 if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
 {
 printf("yes\n");

d53ac9859149087aeac7da50ae5109088d1e7777
krishanakannan <1998starkannan@gmail.com>
2018-03-14 16:21:33 +0530

    Create str num.c

---
+++ str num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char b[10];
+	scanf("%s",b);
+	int k,l,i;
+	scanf("%d",&k);
+	m=strlen(b);
+	for(i=k;i<l;i++)
+	{
+		printf("%c",b[i]);
+	}
+	return 0;
+}

92e4276660f30e748a9a2522925e4666b6713887
krishanakannan <1998starkannan@gmail.com>
2018-03-21 16:14:49 +0530

    Update 2222.py

--- 2222.py
+++ 2222.py
@@ -1,16 +1,16 @@
 
 try:
     import re
-    s = input()
-    match = re.match("^[a-zA-Z]+$",s)
+    n = input()
+    match = re.match("^[a-zA-Z]+$",n)
     if(match):
-        for i in range(len(s)):
-            if(ord(s[i])>119 and ord(s[i])<123):
-                print(chr(ord(s[i])-23), end = "")
-            elif(ord(s[i])>86 and ord(s[i])<91):
-                print(chr(ord(s[i])-23),end = "")
+        for i in range(len(n)):
+            if(ord(n[i])>119 and ord(n[i])<123):
+                print(chr(ord(n[i])-23), end = "")
+            elif(ord(n[i])>86 and ord(n[i])<91):
+                print(chr(ord(n[i])-23),end = "")
             else:
-                print(chr(ord(s[i])+3),end = "")
+                print(chr(ord(n[i])+3),end = "")
     else:
         raise Exception
 except:

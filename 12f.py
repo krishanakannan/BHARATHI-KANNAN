1d247862dadad6661e2c616e930f85fc8cc14263
krishanakannan <1998starkannan@gmail.com>
2018-07-24 12:15:18 +0530

    Create 12f.py

---
+++ 12f.py
@@ -0,0 +1,13 @@
+
+a = int(input(" "))
+b = int(input(" "))
+
+print("Prime numbers between",a ,"and",b,"are:")
+
+for num in range(a,b+ 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
